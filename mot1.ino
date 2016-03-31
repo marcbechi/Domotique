158a8c7865cc4635e120df5acf26165d49f8af3e
marcbechi <bechi.marc@gmail.com>
2016-03-31 11:06:07 +0200

    Update mot1.ino

--- mot1.ino
+++ mot1.ino
@@ -1,16 +1,16 @@
-const int AIA1 = 2;
-const int AIB1 = 3;
-const int BIA1 = 4;
-const int BIB1 = 5;
-const int AIA2 = 6;
-const int AIB2 = 7;
-const int BIA2 = 8;
-const int BIB2 = 9;
-int trig = 12;
-int echo = 11;
+const int AIA1 = 2;//B-1A
+const int AIB1 = 3;//B-1B
+const int BIA1 = 4;//A-1A
+const int BIB1 = 5;//A-1B
+const int AIA2 = 6;//B-2A
+const int AIB2 = 7;//B-2B
+const int BIA2 = 8;//A-2A
+const int BIB2 = 9;//A-2B
+int trig = 12; //variable for ultrason
+int echo = 11; // variable for ultrason
 long lecture_echo;
 long cm;
-byte speed1 = 255;
+byte speed1 = 255; //speed of motor
 
 void setup() {
   Serial.begin(9600);
@@ -34,18 +34,6 @@ void loop() {
   forward();
 }
 
-void backward()
-{
-  analogWrite(AIA1, 0);
-  analogWrite(AIB1, speed1);
-  analogWrite(BIA1, 0);
-  analogWrite(BIB1, speed1);
-  analogWrite(AIA2, 0);
-  analogWrite(AIB2, speed1);
-  analogWrite(BIA2, 0);
-  analogWrite(BIB2, speed1);
-}
-
 void forward()
 {
   analogWrite(AIA1, speed1);
@@ -67,9 +55,8 @@ void ultrason ()
   digitalWrite(trig, LOW);
   lecture_echo = pulseIn(echo, HIGH);
   cm = lecture_echo / 58;
-  Serial.print("Distancem : ");
+  Serial.print("Distance : ");
   Serial.println(cm);
   delay(1000);
 }
-
 
